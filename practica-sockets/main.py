893c54af72735ac48295a188dd0331cfefe36aa7
barksanson <barksansondev@gmail.com>
2023-10-04 13:16:05 +0200

    I think this is more sensible lol

--- practica-sockets/main.py
+++ practica-sockets/main.py
@@ -1,63 +1,43 @@
+from _socket import _RetAddress
+from collections.abc import Callable
 import socketserver
+from socketserver import _AfInetAddress, BaseRequestHandler
 import threading
 import time
+from typing import Any
 
 HOST, PORT = 'localhost', 3000
 
-class WaitingRoom():
+class WaitingRoomTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    daemon_threads = True
     MAX_PLAYERS = 4
 
-    def __init__(self):
-        self.__current_players = 0
-        self.__lock = threading.Lock()
-        self.__is_full = threading.Condition(self.__lock)
-
-    @property
-    def current_players(self):
-        return self.__current_players
-    
-    def wait(self):
-        with self.__lock:
-            while self.__current_players < WaitingRoom.MAX_PLAYERS:
-                self._is_full.wait()
-            self.__is_full.notify()
-
-    def join(self):
-        with self.__lock:
-            self.__current_players += 1
-            return self.__current_players
-
+    def __init__(self, 
+                 server_address, 
+                 RequestHandlerClass, 
+                 bind_and_activate = True):
+        super().__init__(server_address, RequestHandlerClass, bind_and_activate)
 
 class WaitHandler(socketserver.StreamRequestHandler):
-    waiting_room = WaitingRoom()
-    connections = []
+    current_players = 0
+    MAX_PLAYERS = 4
+    lock = threading.Lock()
+    sockets = []
 
-    def handle(self):
-        WaitHandler.connections.append(self)
-        WaitHandler.waiting_room.join()
-        print("Player connected")
-        self.__broadcast(f"One player connected. Remaining players: \
-                         {WaitingRoom.MAX_PLAYERS - WaitHandler.waiting_room.current_players}")
-        
-        WaitHandler.waiting_room.wait()
-        
-        if (WaitHandler.waiting_room.current_players >= WaitingRoom.MAX_PLAYERS):
-            self.__broadcast("Number of players reached. Starting game...")
-            time.sleep(2)
-    
-    def __broadcast(self, msg):
-        for c in WaitHandler.connections:
-            if c != self:
-                try:
-                    c.wfile.write(msg.encode("utf-8"))
-                except Exception as e:
-                    print(e)
+    def handle(self): 
+        with WaitHandler.lock:
+            WaitHandler.sockets.append(self.request) 
+            WaitHandler.current_players += 1
+
+
 
 
 def main():
-    server = socketserver.ThreadingTCPServer((HOST, PORT), WaitHandler)
+    server = WaitingRoomTCPServer((HOST, PORT), WaitHandler) 
+
     try:
         print(f"Server listening on port {PORT}")
+
         server.serve_forever()
     except KeyboardInterrupt:
         pass
