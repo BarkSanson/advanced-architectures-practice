96a50ec929a8f96ba997ab8d7372cfe21d9d388c
barksanson <barksansondev@gmail.com>
2023-10-07 21:54:52 +0200

    Basic implementation of RESTful

--- restful/client.py
+++ restful/client.py
@@ -1,6 +1,50 @@
+import requests
+import random
+import sys
+import time
+import enum
+import json
+
+from server_state import ServerState
+
+API = 'http://localhost:5000'
+
+class ServerState(enum.Enum):
+    WAITING = 0
+    FAILED = 1
+    SUCCESS = 2
 
 def main():
-    pass
+    if len(sys.argv) != 2:
+        print("Incorrect usage")
+        print("Correct syntax: python client.py <id>")
+        return
+    
+    id = sys.argv[1]
+
+    headers = { 'Content-Type': 'application/json' }
+
+    while True:
+        n = random.randint(1, 10)
+        print("Generated random number: {n}")
+
+        data = json.dumps({ "id": id, "number": n})
+        
+        res = requests.post(f'{API}/number', data=data, headers=headers)
+
+        state = res.json()["state"]
+ 
+        while state == ServerState.WAITING.value:
+            res = requests.get(f'{API}/state').json()
+            state = res["state"]
+
+            print(f"Server state {state}")
+
+            time.sleep(5)
+        
+        if state == ServerState.SUCCESS.value:
+            print("Majority!")
+            break            
 
 if __name__ == "__main__":
     main()
--- restful/server.py
+++ restful/server.py
@@ -1,6 +1,54 @@
+from flask import Flask, request, jsonify
 
-def main():
-    pass
+from server_state import ServerState
+
+app = Flask(__name__)
+
+CLIENTS = 3
+numbers = []
+
+state = ServerState.WAITING
+agents_checked = 0
+
+@app.post('/number')
+def number():
+    json = request.get_json()
+
+    if len(numbers) < CLIENTS:
+        numbers.append(json["number"])
+
+    return jsonify({ "state": state.value }), 201
+
+@app.get('/state')
+def state_check():
+    if len(numbers) < CLIENTS:
+        return jsonify({ "state": state.value }), 200
+    
+    check_if_majority()
+
+    return jsonify({ "state": state.value }), 200
+
+
+def check_if_majority():
+    global state
+
+    count = {}
+    max = ('', 0)
+
+    for n in numbers:
+        if n in count:
+            count[n] += 1
+        else:
+            count[n] = 1
+        
+        if count[n] > max[1]:
+            max = (n, count[n])
+    
+    if max[1] > 1:
+        state = ServerState.SUCCESS
+    else:
+        state = ServerState.FAILED
+
 
 if __name__ == "__main__":
-    main()
+    app.run()
