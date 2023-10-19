c4811f6c35b57f4d0acbdd32ffc178629f75391f
barksanson <barksansondev@gmail.com>
2023-10-19 15:25:42 +0200

    Some refactor to Message and added MessageType

--- U5-MaekawaMutexAlgorithm/message.py
+++ U5-MaekawaMutexAlgorithm/message.py
@@ -1,47 +1,87 @@
 import json
+import enum
 
+from dataclasses import dataclass
+
+
+class MessageType(enum.Enum):
+    GREETING = "Greeting"
+    REQUEST = "Request"
+    REPLY = "Reply"
+    RELEASE = "Release"
+
+
+@dataclass
 class Message(object):
-    def __init__(self,
-            msg_type=None,
-            src=None,
-            dest=None,
-            ts=None,
-            data=None,
-            ):
-        self.msg_type = msg_type
-        self.src = src
-        self.dest = dest
-        self.ts = ts
-        self.data = data
+    __msg_type: MessageType
+    __src: int
+    __data: str
+    __dest: int = None
+    __ts: int = None
+
+    @property
+    def msg_type(self):
+        return self.__msg_type
+
+    @msg_type.setter
+    def msg_type(self, msg_type):
+        self.__msg_type = msg_type
+
+    @property
+    def src(self):
+        return self.__src
+
+    @src.setter
+    def src(self, src):
+        self.__src = src
+
+    @property
+    def dest(self):
+        return self.__dest
+
+    @dest.setter
+    def dest(self, dest):
+        self.__dest = dest
+
+    @property
+    def ts(self):
+        return self.__ts
+
+    @ts.setter
+    def ts(self, ts):
+        self.__ts = ts
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, data):
+        self.__data = data
 
     def __json__(self):
-        return dict(msg_type=self.msg_type, 
-            src=self.src,
-            dest=self.dest, 
-            ts=self.ts, 
-            data=self.data)
+        return dict(msg_type=self.msg_type,
+                    src=self.src,
+                    dest=self.dest,
+                    ts=self.ts,
+                    data=self.data)
 
-    def set_type(self, msg_type):
-        self.msg_type = msg_type
+    @staticmethod
+    def from_json(json_str) -> 'Message':
+        obj_dict = json.loads(json_str)
+        msg = Message(MessageType(obj_dict['msg_type']),
+                      obj_dict['src'],
+                      obj_dict['dest'],
+                      obj_dict['ts'],
+                      obj_dict['data'])
 
-    def set_src(self, src):
-        self.src = src
-
-    def set_dest(self, dest):
-        self.dest = dest
-
-    def set_ts(self, ts):
-        self.ts = ts
-
-    def set_data(self, data):
-        self.data = data
+        return msg
 
     def to_json(self):
         obj_dict = dict()
-        obj_dict['msg_type'] = self.msg_type
+        obj_dict['msg_type'] = self.msg_type.value
         obj_dict['src'] = self.src
         obj_dict['dest'] = self.dest
         obj_dict['ts'] = self.ts
         obj_dict['data'] = self.data
         return json.dumps(obj_dict)
-
--- U5-MaekawaMutexAlgorithm/nodeServer.py
+++ U5-MaekawaMutexAlgorithm/nodeServer.py
@@ -4,11 +4,12 @@ import utils
 from message import Message
 import json
 
+
 class NodeServer(Thread):
     def __init__(self, node):
         Thread.__init__(self)
         self.node = node
-    
+
     def run(self):
         self.update()
 
@@ -21,7 +22,7 @@ class NodeServer(Thread):
             (read_sockets, write_sockets, error_sockets) = select.select(
                 self.connection_list, [], [], 5)
             if not (read_sockets or write_sockets or error_sockets):
-                print('NS%i - Timed out'%self.node.id) #force to assert the while condition 
+                print('NS%i - Timed out' % self.node.id)  # force to assert the while condition
             else:
                 for read_socket in read_sockets:
                     if read_socket == self.server_socket:
@@ -32,19 +33,18 @@ class NodeServer(Thread):
                             msg_stream = read_socket.recvfrom(4096)
                             for msg in msg_stream:
                                 try:
-                                    ms = json.loads(str(msg,"utf-8"))
+                                    ms = json.loads(str(msg, "utf-8"))
                                     self.process_message(ms)
                                 except:
-                                    None
+                                    pass
                         except:
                             read_socket.close()
                             self.connection_list.remove(read_socket)
                             continue
-        
+
         self.server_socket.close()
 
     def process_message(self, msg):
-        # Do shomething
-        print("Node_%i receive msg: %s"%(self.node.id,msg))
+        # TODO
 
- 
+        print("Node_%i receive msg: %s" % (self.node.id, msg))
